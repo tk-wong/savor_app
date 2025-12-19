2251abf89bd2eb07b2f95e9a20e28a92ffc63bd5
tk-wong <108210425+tk-wong@users.noreply.github.com>
2025-12-19 21:46:09 +0800

    init model and edit readme.md

---
+++ models/image_generation_model/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from image-generation-model!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/recipe_generation_model/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from recipe-generation-model!")
+
+
+if __name__ == "__main__":
+    main()
