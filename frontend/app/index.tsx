9a5b657b1713c5c3916b13e59d8dd401562ff5d9
Tkwong <108210425+tk-wong@users.noreply.github.com>
2026-01-08 18:45:01 +0800

    enhance login form with state management and styling

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,21 +1,43 @@
-import {Text, TextInput, View} from "react-native";
+import {Text, TextInput, View, TouchableOpacity, StyleSheet} from "react-native";
+import {useState} from "react";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome</Text>
-        <Text>User Name:</Text>
-        <TextInput style={{borderWidth: 1}}/>
-        <Text>Password</Text>
-        <TextInput style={{borderWidth: 1}} secureTextEntry={true}/>
-        <button onClick={()=>{alert("submit")}}>submit</button>
-        <button onClick={()=>{alert("Create account")}}>Create account</button>
-    </View>
-  );
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+
+            <Text>Welcome</Text>
+            <Text>User Name:</Text>
+            <TextInput style={{borderWidth: 1}} placeholder={"User name"} onChangeText={setUsername}/>
+            <Text>Password</Text>
+            <TextInput style={{borderWidth: 1}} secureTextEntry={true} placeholder={"Password"}
+                       onChangeText={setPassword}/>
+            <TouchableOpacity onPress={() => {
+                alert(`name: ${username}, password: ${password}`);
+            }} style={style.button}>
+                <Text>Submit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+                alert("Create account")
+            }} style={style.button}>
+                <Text>Create account</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
+const style = StyleSheet.create(
+    {
+        button: {
+            alignItems: 'center',
+            backgroundColor: '#DDDDDD',
+            padding: 10,
+        },
+    }
+)
