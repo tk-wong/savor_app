80618216131dcf99c733e420e567d2c26e7d7db8
tk-wong <108210425+tk-wong@users.noreply.github.com>
2026-01-23 21:53:56 +0800

    Enhance data processing in process_data.py to sample and format recipe data, and add tqdm for progress tracking

--- models/recipe_generation_model/process_data.py
+++ models/recipe_generation_model/process_data.py
@@ -3,6 +3,8 @@ import random
 import polars as pl
 import json
 
+import tqdm
+
 
 def main():
     dataset_path = "../dataset/RecipeNLG/dataset/full_dataset.csv"
@@ -12,34 +14,30 @@ def main():
         pl.col("directions").str.json_decode(pl.List(pl.String)).alias("directions"),
         pl.col("NER").str.json_decode(pl.List(pl.String)).alias("NER"),
     )
-    print(df.head(10))
-    # seed = 42
-    # random.seed = seed
-    # chosen_data = df['title', 'ingredients', 'directions','NER'].sample(3000,seed=seed).lazy()
-    # all_results = []
-    # for title, ingredients, directions,ner in chosen_data.collect().iter_rows():
-    #
-    #     processed_ingredients = ingredients.replace("[", "").replace("]", "").replace('"', '')
-    #     processed_ner = ner.replace("[", "").replace("]", "").replace('"', '')
-    #     ner_list = processed_ner.split(", ")
-    #     processed_directions = directions.replace("[", "").replace("]", "").replace('"', '').split(", ")
-    #     user_prompt = f"create a recipe using {", ".join(ner_list[:len(ner_list)//2])}."
-    #     final_directions = ""
-    #     for idx, step in enumerate(processed_directions):
-    #         final_directions += f"{idx+1}: {step}\n"
-    #     model_response = \
-    #         (f"Recipe title: {title}\nIngredients:\n{processed_ingredients}\n"
-    #          f"Directions:\n{final_directions}")
-    #     all_results.append([
-    #         {"role": "user", "content": user_prompt},
-    #         {"role": "assistant", "content": model_response}
-    #     ])
-    # result_json = {
-    #     "messages": all_results
-    # }
-    # with open("sample_recipes.jsonl", "w") as f:
-    #     json.dump(result_json, f, indent=2)
-
+    print(f"data format (first 10):{df.head(10)}")
+    print("Start sampling data...")
+    seed = 42
+    chosen_data = df['title', 'ingredients', 'directions','NER'].sample(5000,seed=seed)
+    all_results = []
+    for title, ingredients, directions,ner in chosen_data.iter_rows():
+        user_prompt = f"create a recipe using {", ".join(ner)}."
+        final_directions = ""
+        for idx, step in enumerate(directions):
+            final_directions += f"{idx+1}: {step}\n"
+        model_response = \
+            (f"Recipe title: {title}\nIngredients:\n{'\n'.join(ingredients)}\n"
+             f"Directions:\n{final_directions}")
+        all_results.append([
+            {"role": "user", "content": user_prompt},
+            {"role": "assistant", "content": model_response}
+        ])
+    print("Writing results to sample_recipes.jsonl...")
+    result_json = {
+        "messages": all_results
+    }
+    with open("sample_recipes.jsonl", "w") as f:
+        json.dump(result_json, f, indent=2)
+    print("Done.")
 
 if __name__ == '__main__':
     main()
