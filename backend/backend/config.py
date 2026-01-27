a565f02852f7929481f6a17326160c6de21a4262
Tkwong <108210425+tk-wong@users.noreply.github.com>
2026-01-27 11:15:30 +0800

    Add user authentication blueprint and main application entry point, created login endpoint

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -1,5 +1,5 @@
 import os
-
+import secrets
 from dotenv import load_dotenv
 load_dotenv(".env_dev")
 
@@ -10,3 +10,8 @@ db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+secret_key = secrets.token_hex(32)
+SECRET_KEY = os.getenv("SECRET_KEY", secret_key) # generate a random one if not set
--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from backend import create_app
-app = create_app()
-
-@app.route("/")
-def index():
-    return "Hello, World!"
-
-
-app.run()
