b57f1d978b8c1c928ad9b27a4d3d0fb28f448e63
Tkwong <108210425+tk-wong@users.noreply.github.com>
2026-01-27 22:10:55 +0800

    Implement user authentication setup with database creation and user model integration Test case for login is added

---
+++ backend/test/test_config.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+env_load_status = load_dotenv("test/.env_test")
+if not env_load_status:
+    raise FileNotFoundError("Could not load .env_test file")
+
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/savor_test"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = 'dev' # override for testing purposes
+
+TESTING = True
--- backend/test/user_test.py
+++ backend/test/user_test.py
@@ -1,3 +1,66 @@
 import pytest
-from backend import user
-from backend.database import User
+from werkzeug.security import generate_password_hash
+
+from backend import create_app
+from sqlalchemy_utils import drop_database
+from backend.user_model import User
+from dotenv import load_dotenv
+from sqlalchemy.orm import close_all_sessions
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv(".env_test")
+
+@pytest.fixture()
+def app():
+    app = create_app(config="../test/test_config.py")
+    from backend.database import db
+    # sample_user = User(email="sample@test.com", username="Sample User",password_hash=generate_password_hash("testing"))
+    with app.app_context():
+        db.create_all()
+    yield app
+    # cleanup the database
+    with app.app_context():
+        from backend.database import db
+        db.drop_all()
+        close_all_sessions()
+        db.engine.dispose()
+    drop_database(app.config['SQLALCHEMY_DATABASE_URI'])
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+@pytest.fixture()
+def sample_user(app):
+    with app.app_context():
+        from backend.database import db
+        user = User(email="example@abc.com", username="Example User", password_hash=generate_password_hash("testing"))
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+def test_index(client):
+    response = client.get("/user")
+    assert response.status_code == 200
+    assert response.get_json() == {'message': 'User endpoint'}
+
+def test_login(client, sample_user):
+    response = client.post("/user/login", data={"email": "example@abc.com", "password": "testing"})
+    assert response.status_code == 200
+    assert response.get_json() == {'message': 'Welcome back, Example User!'}
+
+def test_invalid_user(client,sample_user):
+    response = client.post("/user/login", data={"email": "notexist@abc.com", "password": "testing"})
+    assert response.status_code == 401
+    assert response.get_json() == {'message': 'Invalid credentials'}
+
+def test_invalid_password(client, sample_user):
+    response = client.post("/user/login", data={"email": "example@abc.com", "password": "wrong_password"})
+    assert response.status_code == 401
+    assert response.get_json() == {'message': 'Invalid credentials'}
