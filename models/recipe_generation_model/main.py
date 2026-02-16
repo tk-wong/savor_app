45fcfc3bf9f3859afaa22e535fdfef21b06b8fff
tk-wong <108210425+tk-wong@users.noreply.github.com>
2026-02-16 22:18:59 +0800

    Add Flask integration for recipe generation endpoint in main.py

--- models/recipe_generation_model/main.py
+++ models/recipe_generation_model/main.py
@@ -1,5 +1,7 @@
 import time
 
+import flask
+from flask import Flask
 from langchain_ollama import OllamaEmbeddings
 from langchain_ollama.llms import OllamaLLM
 
@@ -8,20 +10,23 @@ from recipe_retriever import RecipeRetriever
 
 
 def main():
-    model = OllamaLLM(model="qwen3:4b")
+    app = Flask(__name__)
+    model = OllamaLLM(model="qwen3:1.7b")
     classification_model = OllamaLLM(model="qwen3:0.6b")
     embedding_model = OllamaEmbeddings(model="qwen3-embedding:0.6b")
     recipe_retriever = RecipeRetriever(env_path="./.env_dev", dataset_path="../dataset/RecipeNLG/dataset/full_dataset.csv",
                                        embeddings_model=embedding_model)
     recipe_assistant = RecipeAssistant(model, classification_model, recipe_retriever)
-    request = "create a recipe that use beef for mapo tofu"
-    last = time.time()
-    result = recipe_assistant.handle_request(request)
-    for chunk in result:
-        print(chunk, end="", flush=True)
-    end = time.time()
-    print(f"\nTime taken: {end - last:.2f} seconds")
 
+
+    @app.route('/recipe_generation', methods=['POST'])
+    def recipe_generation():
+        request = flask.request.json.get('createRequest')
+        if not request:
+            return {"error": "createRequest is required"}, 400
+        result = recipe_assistant.handle_request(request)
+        return flask.Response(result, mimetype="application/json")
+    app.run(port=5010, debug=True)
 
 if __name__ == '__main__':
     main()
