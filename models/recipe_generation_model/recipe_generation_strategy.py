c1b96a6f852492a91cc9106b16d54e6c10d7f637
tk-wong <108210425+tk-wong@users.noreply.github.com>
2026-02-22 23:01:43 +0800

    change the format of recipe so that it can generate ingredient in JSON format

--- models/recipe_generation_model/recipe_generation_strategy.py
+++ models/recipe_generation_model/recipe_generation_strategy.py
@@ -17,7 +17,7 @@ class RecipeGenerationStrategy(ChainStrategy):
         "prompt_type": "recipe",
         "recipe": {{
         "title": "the title of the recipe you created",
-        "ingredients": ["a list of ingredients needed for the recipe, which should be in a simple and concise format, and only include the ingredients that are necessary for the recipe"],
+        "ingredients": ["a list of ingredients in JSON format: {{"ingredient_name": "the name of the ingredient in singular form. Specify how to process it like 'sliced'", "quantity": "the quantity of the ingredient needed for the recipe. The quantity should be in a standard format, such as '1 cup', '2 tablespoons', '3 ounces', etc. Do not add ingredient name"}}.],
         "directions": ["a list of detailed directions for cooking the dish, which should be clear and easy to follow even for a beginner, and should include any necessary cooking tips or techniques that are relevant to the recipe. 
         Do not include the index of the direction step in the direction description. You can use markdown format to make the directions more clear and organized."],
         "description": "a detailed description of the flavor and texture of the dish after it is finished, which should be vivid and appetizing, and should give the user a clear idea of what to expect from the dish when they cook it",
@@ -30,10 +30,10 @@ class RecipeGenerationStrategy(ChainStrategy):
 
     def build_chain(self, llm, request_runnable):
         from langchain_core.prompts import ChatPromptTemplate
-        recipe_generation_prompt = ChatPromptTemplate.from_template(self.recipe_creation_template)
+        recipe_generation_prompt = ChatPromptTemplate.from_template(
+            self.recipe_creation_template)
         recipe_generation_chain = request_runnable | recipe_generation_prompt | llm
         return recipe_generation_chain
-
 
     def request_type(self):
         return "recipe"
