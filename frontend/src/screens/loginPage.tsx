5e9765dd19908e23404819cb84f591626d7a9fa8
tk-wong <108210425+tk-wong@users.noreply.github.com>
2026-03-14 14:44:32 +0800

    feat: Enhance login page styling and structure with global CSS utilities

--- frontend/src/screens/loginPage.tsx
+++ frontend/src/screens/loginPage.tsx
@@ -1,16 +1,14 @@
-
-import { SafeAreaView } from "react-native-safe-area-context";
-import { useState } from "react";
-import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { login } from "../api";
-import { router } from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {useState} from "react";
+import {Alert, StyleSheet, Text, TextInput, TouchableOpacity} from "react-native";
+import {login} from "../api";
+import {router, Stack} from "expo-router";
 
 export default function LoginPage() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
 
-    
     const loginHandler = () => {
         if (!email || !password) {
             Alert.alert("Error", "Please enter both email and password");
@@ -25,40 +23,44 @@ export default function LoginPage() {
         });
     }
     return (
-        <SafeAreaView
-            // style={{
-            //     flex: 1,
-            //     justifyContent: "center",
-            //     alignItems: "center",
-            // }}
-        >
-            <Text>Welcome</Text>
-            <Text>Email:</Text>
-            <TextInput style={{ borderWidth: 1 }} placeholder={"User name"} onChangeText={setEmail}
-                id={"username_input"} />
-            <Text>Password</Text>
-            <TextInput style={{ borderWidth: 1 }} secureTextEntry={true} placeholder={"Password"}
-                onChangeText={setPassword} id={"password_input"} />
-            <TouchableOpacity onPress={
-                loginHandler
-            }
-                style={style.button}>
-                <Text>Submit</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {
-                // const message = "Create account"
-                router.navigate("/createUserPage")
-            }} style={style.button}>
-                <Text>Create account</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {
-                // for debugging
-                router.push("/functionTestingPage")
-            }} style={style.button}>
-                <Text>Function testing page</Text>
-            </TouchableOpacity>
+        <>
+            <Stack.Screen
+                options={{
+                    headerShown: true,
+                    title: "Welcome",
+                    headerTitleAlign: "center",
+                }}
 
-        </SafeAreaView >
+            />
+            <SafeAreaView className={"flex gap-4 mx-auto w-full px-4"}
+            >
+
+                <Text>Email:</Text>
+                <TextInput className={"global-text-input"} placeholder={"User name"} onChangeText={setEmail}
+                           id={"username_input"}/>
+                <Text>Password</Text>
+                <TextInput className={"global-text-input"} secureTextEntry={true} placeholder={"Password"}
+                           onChangeText={setPassword} id={"password_input"}/>
+                <TouchableOpacity onPress={
+                    loginHandler
+                } className={"global-button"}>
+                    <Text>Submit</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {
+                    // const message = "Create account"
+                    router.navigate("/createUserPage")
+                }} className={"global-button"}>
+                    <Text>Create account</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {
+                    // for debugging
+                    router.push("/functionTestingPage")
+                }} className={"global-button"}>
+                    <Text>Function testing page</Text>
+                </TouchableOpacity>
+
+            </SafeAreaView>
+        </>
     );
 }
 const style = StyleSheet.create(
