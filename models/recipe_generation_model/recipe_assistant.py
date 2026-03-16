48b4c0729263de9a4b656d2e83dab70d0b65785b
tk-wong <108210425+tk-wong@users.noreply.github.com>
2026-03-16 19:54:00 +0800

    feat: enhance recipe assistant with user and group ID handling, improve session management, and update dependencies

--- models/recipe_generation_model/recipe_assistant.py
+++ models/recipe_generation_model/recipe_assistant.py
@@ -3,9 +3,9 @@ from operator import itemgetter
 
 import psycopg
 from flask import Flask
-from langchain_core.chat_history import InMemoryChatMessageHistory
-from langchain_core.runnables import RunnableParallel, RunnableWithMessageHistory, RunnableLambda, RunnablePassthrough
+from langchain_core.runnables import RunnableParallel, RunnableWithMessageHistory, RunnableLambda
 from langchain_ollama import OllamaLLM
+from langchain_postgres import PostgresChatMessageHistory
 
 from classification_strategy import ClassificationStrategy
 from question_strategy import QuestionStrategy
@@ -15,7 +15,7 @@ from recipe_retriever import RecipeRetriever
 
 class RecipeAssistant:
     def __init__(self, generation_model: OllamaLLM, classification_model: OllamaLLM, recipe_retriever: RecipeRetriever,
-                 db_path: str, app: Flask, table_name: str = "chat_history"):
+                 db_path: str, app: Flask, table_name: str = "llm_chat_history"):
         self.model_strategies = {
             "recipe": RecipeGenerationStrategy(),
             "question": QuestionStrategy(),
@@ -43,19 +43,16 @@ class RecipeAssistant:
             }
         )
         )
-        self.classify_runnable =RunnableParallel(
-                    {
-                        "request": itemgetter("request"),
-                        "chat_history": itemgetter("chat_history"),
-                    }
+        self.classify_runnable = RunnableParallel(
+            {
+                "request": itemgetter("request"),
+                "chat_history": itemgetter("chat_history"),
+            }
         )
 
-        self.store = {} # in-memory store for chat history, keyed by session_id
-        self.session_id = uuid.uuid5(uuid.UUID('12345678-1234-5678-1234-567812345678'), "default_session")
-
-    def classify(self, request):
+    def classify(self, request, session_id):
         classifier_chain = self.model_strategies["classification"].build_chain(self.classification_model,
-                                                                               self.classify_runnable )
+                                                                               self.classify_runnable)
         recipe_create_words = [
             "create", "make", "generate", "write", "give me", "provide", "i want", "need", "suggest", "develop",
             "formulate", "cook", "recipe", "recipes", "prepare", "example"
@@ -74,12 +71,16 @@ class RecipeAssistant:
             return "recipe"
         if ask_question:
             return "question"
-        result = classifier_chain.invoke({"request": request, "chat_history": self.store.get(self.session_id)})  # cannot decide based on keywords, use model to classify
+        result = classifier_chain.invoke({"request": request, "chat_history": self.get_conversation_history(
+            session_id)})  # cannot decide based on keywords, use model to classify
         return result.lower().strip()
 
-    def handle_request(self, request):
+    def handle_request(self, request, user_id, group_id):
         try:
-            classification = self.classify(request)
+            uuid_namespace = uuid.NAMESPACE_DNS
+            session_str = f"{user_id}-{group_id}"
+            session_id = str(uuid.uuid5(uuid_namespace, session_str))
+            classification = self.classify(request, session_id)
             print(f"Classification result: {classification}")
             strategy = self.model_strategies.get(classification)
             if strategy is None:
@@ -92,26 +93,26 @@ class RecipeAssistant:
                 history_messages_key="chat_history",
             )
 
-
             result = chain_with_history.stream({"request": request},
-                                               dict(configurable={"session_id": self.session_id}),
+                                               config={"configurable": {"session_id": session_id}},
                                                )
             for chunk in result:
                 yield chunk
-            print(self.store)
         except Exception as e:
             # self.app.logger.error(f"Error handling request: {e}\n Traceback: {e.__traceback__}")
             yield "{\"error\": \"An error occurred while processing the request.\"}"
             raise e
 
     def get_conversation_history(self, session_id):
-        # return  PostgresChatMessageHistory(
-        #     self.table_name,
-        #     session_id,
-        #     sync_connection=self.db_connection,
-        #     # connection_string=self.db_path,
-        # )
-        if session_id not in self.store:
-            self.store[session_id] = InMemoryChatMessageHistory()
-        return self.store[session_id]
+        history = PostgresChatMessageHistory(
+            self.table_name,
+            session_id,
+            sync_connection=self.db_connection,
+            # connection_string=self.db_path,
+        )
+        history.create_tables(self.db_connection, self.table_name)
+        return history
+        # if session_id not in self.store:
+        #     self.store[session_id] = InMemoryChatMessageHistory()
+        # return self.store[session_id]
         # return InMemoryChatMessageHistory()
